51422b8a01eb45693894da5a47d3e67e1819cf1a
Filippo Mei <fmei@neutrino-01.cloudcnaf>
2025-09-26 11:47:18 +0200

    more condor scripts

--- condor_scripts/launch_merge_edepsim_tree.sh
+++ condor_scripts/launch_merge_edepsim_tree.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT_FILE_PATTERN="/storage/gpfs_data/neutrino/SAND-LAr/SAND-LAr-EDEPSIM-PROD/new-numu-CC-QE-in-GRAIN/grain_numu_ccqe/grain_numu_ccqe_*/sand-events.*.edep.root"
+INPUT_FILE_PATTERN="/storage/gpfs_data/neutrino/SAND-LAr/SAND-LAr-EDEPSIM-PROD/new-numu-CC-QE-in-GRAIN/grain_numu_ccqe/grain_numu_ccqe_*/skimmed-sand-events.*.edep.root"
 OUTPUT_FILE="/storage/gpfs_data/neutrino/SAND-LAr/SAND-LAr-EDEPSIM-PROD/new-numu-CC-QE-in-GRAIN/grain_numu_ccqe/merged-skimmed-sand-events.edep.root"
 
 source source_libs_neutrino01.sh
